51c9aaa363a8004436e93bc8039a5e6b45fae3c6
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-10-28 17:03:25 +0100

    modification App.tsx et index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import Gentils from "./Compenents/Gentils";
 import Mechants from "./Compenents/Mechants";
-import Header from "./Compenents/header";
-import Footer from "./Compenents/footer";
+import Header from "./Compenents/Header";
+import Footer from "./Compenents/Footer";
 import CountDragonBall from "./Compenents/CountDragonBall";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
       <div>
