34c968b1f303cfbed8dc94999736a3eea696b934
maldonae <maldonadoeric69120@gmail.com>
2024-10-28 16:00:08 +0100

    création des composants gentils et méchants + début css

--- src/Compenents/Gentils.tsx
+++ src/Compenents/Gentils.tsx
@@ -1,9 +1,20 @@
-function Gentils() {
+interface gentilCardprops {
+  gentilsData: {
+    name: string;
+    imgSrc?: string;
+  };
+}
+
+function gentilsCard({ gentilsData }: gentilCardprops) {
   return (
     <section>
-
-
+      {gentilsData.imgSrc !== undefined ? (
+        <img src={gentilsData.imgSrc} alt={gentilsData.name} />
+      ) : (
+        <p>"???"</p>
+      )}
+      <figcaption>{gentilsData.name}</figcaption>
     </section>
   );
 }
-export default Gentils;
+export default gentilsCard;
